64fea2faf930bf2c24c957e84d9ebae2009e0d77
dataplayer12 <jaiyamsharma@gmail.com>
2022-02-13 15:28:41 +0900

    save raw, control cooker and basic MLOps

--- cooklive.py
+++ cooklive.py
@@ -1,5 +1,14 @@
 from cooknet import CookNet
+import sys
 
-net=CookNet(2,None,'./models/oldcook_50.pth')
+net=CookNet(2,None,'./models/20220123_cook_99.pth')#.half()
 
-net.inferlive(save_path=None, really_not_steaming=True)
+#net.inferlive(save_path='../livecook0127.mp4', really_not_steaming=False)
+#net.inferlive(cam='../cooker0127.mp4', save_path='../infer0127_99_full.mp4') #, cropdims=[202,0,320,240])
+
+net.inferlive(save_path=sys.argv[1],
+	really_not_steaming=False, #this is not a MLOps-ish run
+	cropdims=None,
+	save_raw=True,
+	do_control=True
+	)
