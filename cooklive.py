113fee95f9a03b904b68334afbe66abf8e724947
dataplayer12 <jaiyamsharma@gmail.com>
2022-03-18 00:48:52 +0900

    minor changes

--- cooklive.py
+++ cooklive.py
@@ -6,7 +6,9 @@ net=CookNet(2,None,'./models/20220123_cook_99.pth')#.half()
 #net.inferlive(save_path='../livecook0127.mp4', really_not_steaming=False)
 #net.inferlive(cam='../cooker0127.mp4', save_path='../infer0127_99_full.mp4') #, cropdims=[202,0,320,240])
 
-net.inferlive(save_path=sys.argv[1],
+net.inferlive(
+	save_path=sys.argv[1],
+	temp_path='data/op1002_t.png', #'data/0930_t.png', 
 	really_not_steaming=False, #this is not a MLOps-ish run
 	cropdims=None,
 	save_raw=True,
