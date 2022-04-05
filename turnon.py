7d4003e9de26238ce9fcacd2072e44fde7547104
dataplayer12 <jaiyamsharma@gmail.com>
2022-04-05 16:02:46 +0900

    turn GPIO on/off

--- turnon.py
+++ turnon.py
@@ -1,6 +1,14 @@
 from RPi import GPIO
+import sys
+if len(sys.argv)>1:
+	level=GPIO.LOW
+else:
+	level=GPIO.HIGH
+
 GPIO.setmode(GPIO.BOARD)
 output_pin=18
 GPIO.setup(output_pin, GPIO.OUT)
-GPIO.output(output_pin,GPIO.HIGH)
-print('GPIO set up successfully and turned on')
+GPIO.output(output_pin,level)
+lstr='on' if level==GPIO.HIGH else 'off'
+
+print('GPIO set up successfully and turned {}'.format(lstr))
