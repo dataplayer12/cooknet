386094f9ea86ac63222114044804165c984db17a
dataplayer12 <jaiyamsharma@gmail.com>
2022-04-05 16:02:16 +0900

    improved shell script

--- cook.sh
+++ cook.sh
@@ -2,4 +2,18 @@
 
 cd cooknet
 
-DISPLAY=:0 nohup python3 -u cooklive.py ../cook_$(date -Idate).mp4 &> ../log_$(date -Idate).txt &
+export dname="../on_$(date -Idate)"
+
+echo $dname
+mkdir "$dname"
+
+if [ $? -eq 1 ]
+then
+export dname=${dname}_1 #../cook_$(date -Idate)_1 #$(dname)_1
+#echo "here"
+mkdir -p $dname
+echo "Saving files to $dname";
+fi
+
+
+DISPLAY=:0 nohup python3 -u cooklive.py $dname/cook_$(date -Idate).mp4 &> $dname/log_$(date -Idate).txt &
